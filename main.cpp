8f8e0c56369362333b734703c3b49c0ac09c8fb4
Jamal Ait Chdid <jait-chd@c2r7p3.1337.ma>
2026-05-10 10:21:24 +0100

    start proj

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+// we should create 2 calsses 1 for server and 1 for client 
+// we also need too include header files
+// server class : will contain all info about serveer also will have a victor of clients to keep track all clients and manage their requests
+// client class on other hand will conatin all infos about a specific client inside the server
+class MyClass {
+	private:
+		int port;
+		string pass;
+	public:
+	    	MyClass() : port(0000), pass("0000") {
+			cout << "Constructor Called \n";
+		}                               
+    		MyClass(const MyClass& other)  {
+			cout << "Cpy Constructor Called \n";
+		}           
+    		MyClass& operator=(const MyClass& other); 
+    		~MyClass() {
+			cout << "Destructor Called \n";
+		} 
+		int Get_port(int port_x) {
+			return port_x;
+		}
+		string Get_pass(string pass_x) {
+			return pass_x;
+		}		
+
+};
+
+
+
+int main(int counter , char **vectors) {
+	MyClass Obj;
+	
+	if(counter < 3 || counter > 3)
+		return 1;
+	int port = atoi(vectors[1]);
+	string pass = vectors[2];
+	
+	cout << Obj.Get_port(port) << endl;
+	cout << Obj.Get_pass(pass) << endl;
+	if(port > 2147483647 || port < -2147483648)	
+		return -1;
+
+	// for the server role
+	// socket() ->  ioctl()  -> bind()  -> listen()  -> FD_ZERO()  -> FD_SET()  -> select() 
+	// --> close() cuz of timeout (aslo clode all open sockets and program ends)   -> FD_ISSET() listening socket 
+	// 1-> accept() -> FD_SET() 
+	// 2-> recv()  --> 11-> close() eof -> FD_CLR() 12-->send() recv data
+	// and the loop goes on
+	//cout << port << " " << pass << endl;
+	return 0;
+}
